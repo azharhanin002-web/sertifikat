6bb29dfa53cd112c674ae29499c81e219eacf628
Azhar Hanin <azharhanin002@gmail.com>
2025-12-22 08:24:28 +0700

    Fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,16 @@ await import("./env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  
+  // Tambahkan ini agar gambar dari Sanity bisa muncul
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 };
+
 export default config;
